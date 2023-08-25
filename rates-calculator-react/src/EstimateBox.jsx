a98cac70d4627ef264413b1f66a503078e1ede59
Ben Weibel <weibel.b@gmail.com>
2023-08-25 10:24:24 -0700

    Styling fixes and cleanup

--- rates-calculator-react/src/EstimateBox.jsx
+++ rates-calculator-react/src/EstimateBox.jsx
@@ -29,7 +29,7 @@ const EstimateBox = () => {
   }, []);
 
   return (
-    <div className="estimate-box">
+    <div className="estimate-box has-shadow">
       <h3 className="title">Estimated Cost</h3>
       <h4>Estimated Delivery</h4><h4>Estimated Pickup</h4><h4>Estimated Monthly</h4>
       <span className="cost">${ deliveryCost }</span><span className="cost">${ pickupCost }</span><span className="cost">${ monthlyCost }</span>
