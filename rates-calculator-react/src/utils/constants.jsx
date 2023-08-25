365569c333e3dc90630c19e2a08fc8cb1d55282a
Ben Weibel <weibel.b@gmail.com>
2023-08-25 07:09:22 -0700

    Add yard lat/lng placeholders

--- rates-calculator-react/src/utils/constants.jsx
+++ rates-calculator-react/src/utils/constants.jsx
@@ -1,19 +1,25 @@
 export const STORAGE_TYPES = {
     1: {
-      title: "I Need Storage",
+    cardTitle: "I Need Storage",
+      storageTitle: "At Home Storage",
       description: "We deliver the box to your property for your use and pick it up when your done",
       icon: "./how-it-works-3.png"
     },
     2: {
-      title: "I'm Moving",
+      cardTitle: "I'm Moving",
+      storageTitle: "Transport",
       description: "We deliver the box to your property you fill it, we pick it up and deliver it to your new address",
       icon: "./how-it-works-4.png"
     },
     3: {
-      title: "Off-site Storage",
+      cardTitle: "On Site Storage",
+      storageTitle: "On Site Storage",
       description: "We deliver the box to your property you fill it, we pick it up and store it at our facility until you are ready for it again",
       icon: "./how-it-works-4.png"
     }
   };
   
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyBNh4-tMKKCd0LVTkNbNaRqtAOXqyJQLok';
+
+export const PDX_YARD_LATLONG = { lat: 45.384044684540335, lng: -122.78474009882885 }
+export const RDM_YARD_LATLONG = { lat: 44.285720, lng: -121.157360 }
